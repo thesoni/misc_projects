572108b1a09eaf882f013f33b913eefd89affbdf
thesoni <41269759+thesoni@users.noreply.github.com>
2020-11-29 12:35:54 -0500

    Update typeracer.py
    #  Update:  They changed the XPath.  Right click->Inspect->Copy->XPath

--- typeRacerBot/typeracer.py
+++ typeRacerBot/typeracer.py
@@ -1,6 +1,10 @@
 # 1) pip install selenium
 # 2) Download ChromeDriver from https://chromedriver.chromium.org/
 # 3) Put above folder PATH into the main() code at very bottom
+#
+#  Update:  They changed the XPath.  Right click->Inspect->Copy->XPath
+#           
+#
 
 import time
 from selenium import webdriver
@@ -11,16 +15,17 @@ from selenium.webdriver.support import expected_conditions as EC
 def typeGame(driver):
     URL = 'https://play.typeracer.com/'
     driver.get(URL)
-    time.sleep(1)
+    time.sleep(5)
     
     # Click start game
-    driver.find_element_by_xpath('//*[@id="dUI"]/table/tbody/tr[2]/td[2]/div/div[1]/div/table/tbody/tr[2]/td/table/tbody/tr/td[2]/table/tbody/tr[1]/td/a').click()
+    #driver.find_element_by_xpath('//*[@id="dUI"]/table/tbody/tr[2]/td[2]/div/div[1]/div/table/tbody/tr[2]/td/table/tbody/tr/td[2]/table/tbody/tr[1]/td/a').click()
+    driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[1]/div[2]/table/tbody/tr[2]/td[2]/div/div[1]/div/div[1]/div/a').click()
     
-    time.sleep(3)
+    time.sleep(5)
     
     # Get the msg to be typed
     #msg = driver.find_element_by_xpath('//*[@id="gwt-uid-15"]/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr[1]/td/div/div/span[1]').text
-    Xp = '/html/body/div[1]/div/div[1]/div[2]/table/tbody/tr[2]/td[2]/div/div[1]/div/table/tbody/tr[2]/td[3]/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr[1]/td/div/div/span['
+    Xp = '//*[@id="gwt-uid-17"]/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr[1]/td/div/div/span['
     msg2 = ''
     spans = []
     
@@ -37,7 +42,9 @@ def typeGame(driver):
         except:
             maxSpan = i
             break
-        
+
+    print(spans)
+    
     # Assemble the message, and add the missing space 
     # If there are 3 spans, maxSpan crashed the try() at 4
     # and it needs a space after span 2, hence 4-2
@@ -59,7 +66,7 @@ def typeGame(driver):
     
 def main():
     #driver = webdriver.Chrome()
-    driver = webdriver.Chrome(executable_path=r"C:\Users\ssoni\Documents\#installs\chromedriver_win32\chromedriver.exe")
+    driver = webdriver.Chrome(executable_path=r"C:\Users\sidds.DESKTOP-N255TP6\Documents\install\chromedriver_win32\chromedriver.exe")
     driver.maximize_window()
     
     typeGame(driver)
